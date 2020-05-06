df0362e6994e2cfbb8fa711752c20a79de0419aa
LEE-HOON <55974451+LEE-HOON@users.noreply.github.com>
2020-05-06 23:35:47 +0900

    05

---
+++ 4/quiz4.js
@@ -0,0 +1,28 @@
+const $box = document.getElementById('box');
+
+// let offset = { x : 0, y :  0} ;
+let isDown = false;
+
+$box.addEventListener('mousedown', (event) => {
+    isDown = true;
+    // console.log('mousedown');
+    console.log($box.offsetLeft);
+    // offset.x = $box.offsetLeft - event.clientX;
+    // offset.y = $box.offsetTop - event.clientY;
+});
+
+$box.addEventListener('mouseup' , (event) => {
+    isDown = false;
+    // console.log('mouseup');
+});
+
+document.body.addEventListener('mousemove', (event) =>{
+    console.log('mousemove');
+    if(!isDown) return;
+    console.log(event.clientX, event.clientY);
+
+    $box.style.left = event.clientX;
+    $box.style.top = event.clientY;
+    // $box.style.left = event.clientX + offset.x;
+    // $box.style.top = event.clientY + offset.y;
+});
